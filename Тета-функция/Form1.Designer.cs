90f42717b7973e089e9011c957a9ac01501a9652
bosonogiyMONAX <aleksandr1998d@yandex.ru>
2019-04-15 17:05:04 +0300

    Changes

---
+++ Тета-функция/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Тета_функция
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.Blue;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            series3.Legend = "Legend1";
+            series3.Name = "sig < pi";
+            series3.YValuesPerPoint = 2;
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "sig > pi";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(781, 345);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "q = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 400);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Тета-функция/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Тета_функция
+{
+    public partial class Form1 : Form
+    {
+        const int k_max = 500;
+        const double buf_max = 80;
+        const double pi = Math.PI;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Theta(double t, double sig, int num) //num - номер способа
+        {
+            double sum, buf, xx, buf2, q;
+            int k;
+            if (num == 1)
+            {
+                sum = 1;
+                k = 0;
+                do
+                {
+                    k++;
+                    buf = (k * k) / (2 * sig * sig);
+                    sum += 2 * Math.Exp(-buf) * Math.Cos(2 * k * t);
+                } while ((buf < buf_max) && (k < k_max));
+                q = Math.Exp(-1 / (2 * sig * sig)); //считаем и выводим q в текстбокс
+                textBox1.Text = q.ToString();
+                return sum;
+            }
+            else
+            {
+                k = (int)Math.Round(Math.Abs(t) / pi);
+                xx = Math.Abs(t) - k * pi;
+                sum = Math.Exp(-(sig * xx * xx));
+                k = 0;
+                do
+                {
+                    k++;
+                    buf = sig * Math.Pow(xx + pi * k, 2);
+                    buf2 = (sig * sig * Math.Pow(xx - 2 * pi * k, 2)) / 2;
+                    //buf2 = sig * Math.Pow(xx - pi * k, 2); 
+                    sum += Math.Exp(-((0.5 * sig * sig) * Math.Pow(t - 2 * pi * k, 2)));
+                    //sum += Math.Exp(-buf) + Math.Exp(-buf2);
+                } while (((buf < buf_max) || (buf2 < buf_max)) && (k < k_max));
+
+                q = Math.Exp(-1 / (2 * sig * sig)); //считаем и выводим q в текстбокс
+                textBox1.Text = q.ToString();
+                return Math.Sqrt(2 * pi) * sig * sum;
+                //return Math.Sqrt(pi * sig) * sum;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(double i = -4; i<=4; i = i+0.01)
+            {
+                chart1.Series[0].Points.AddXY(i + 0.01, Theta(i, 7, 1));
+                chart1.Series[1].Points.AddXY(i + 0.01, Theta(i, 7, 2));
+            }
+        }
+    }
+}
